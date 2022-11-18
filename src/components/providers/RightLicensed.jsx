7ab09783d4977da0f03bff285452793d5fdd9dcb
Bertrand <mutanganabertrand@gmail.com>
2022-11-18 20:07:33 +0200

    Add the sickness

--- src/components/providers/RightLicensed.jsx
+++ src/components/providers/RightLicensed.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { BsCheckLg } from 'react-icons/bs';
+
+const sickness = ['Depression', 'Anxiety', 'Stress', 'Relationship', 'Family', 'Work', 'School', 'Anxiety & Stress', 'Anxiety & Stress', 'Anxiety & Stress','Anxiety & Stress', 'Anxiety & Stress', 'Anxiety & Stress', 'Anxiety & Stress', 'Anxiety & Stress', 'Anxiety & Stress', 'Anxiety & Stress'];
 
 const RightLicenced = () => {
   return (
-    <div>RightLicenced</div>
+    <div className='w-full  md:w-[50%]'>
+      <h2 className='heading colored leading-[1] my-10'>Meet our network of Licenced providers</h2>
+      <p className='mt-1'>We have a network of licenced providers who are ready to help you with your mental health needs. </p>
+      <div>
+        <ul className='grid grid-cols-2 grid-flow-row justify-items-start  my-3 mx-4 md:grid-cols-3'>
+          {
+            sickness.map((item) => (
+              <li className='flex my-2 justify-center items-center gap-2 text-2xl' key={item}>
+                <BsCheckLg className='text-sm text-blue-500' />
+                {item}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- src/components/providers/index.jsx
+++ src/components/providers/index.jsx
@@ -5,9 +5,9 @@ import RightLicenced from './RightLicensed';
 
 const Providers = () => {
   return (
-    <section className='flex flex-col m-2 p-3 space-y-6 items-center md:flex-row justify-around'>
-        <SlideShow />
+    <section className='flex flex-col-reverse m-2 p-3 space-y-10  items-center justify-center md:flex-row items-stretch justify-around'>
         <RightLicenced />
+        <SlideShow />
     </section>
   )
 }
