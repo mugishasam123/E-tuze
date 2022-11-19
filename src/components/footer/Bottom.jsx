ec213cb6989f74766328d4bcf9a0ba38671f154b
Bertrand <mutanganabertrand@gmail.com>
2022-11-19 16:00:30 +0200

    Add botom line

--- src/components/footer/Bottom.jsx
+++ src/components/footer/Bottom.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Bottom = () => {
   return (
-    <div>Bottom</div>
+    <div>
+        <p className='text-center text-xl text-white'>If you are in a life threatening situation - don't use this site. <a href='#' className='text-blue-900 hover:opacity-60'>Call +1 (800) 273-8255</a> to get immediate help.</p>
+        <hr />
+        <p className='text-center text-xl text-white'>© 2021 E-Tuze. All rights reserved.</p>
+    </div>
   )
 }
 
