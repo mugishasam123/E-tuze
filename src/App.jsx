2db0afb5c30c8c582cf9d89c6182d3755f1bff13
Bertrand <mutanganabertrand@gmail.com>
2022-11-21 13:37:33 +0200

    Add provider login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/client/Login';
+import ProviderLogin from './pages/provider/Login';
 
 const App = () => (
   <div className="App">
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/client/login" element={<Login />} />
+      <Route path="/provider/login" element={<ProviderLogin />} />
     </Routes>
   </div>
 );
