fac798418a3b0d63fdae8fc6a20597200b850bcb
Bertrand <mutanganabertrand@gmail.com>
2022-11-22 18:30:13 +0200

    Add dashboard routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/home/Home';
 import Login from './pages/client/Login';
 import ProviderLogin from './pages/provider/Login';
 import Register from './pages/provider/Register';
+import ProviderDashboard from './pages/provider/dashboard';
 import GetStarted from './pages/getStarted/index';
 import Questionaire from './pages/questionaire/index';
 import Thank from './pages/Thank/Thank';
@@ -14,6 +15,7 @@ const App = () => (
       <Route path="/client/login" element={<Login />} />
       <Route path="/provider/login" element={<ProviderLogin />} />
       <Route path="/provider/register" element={<Register />} />
+      <Route path="/provider/dashboard" element={<ProviderDashboard />} />
       <Route path="/get-started" element={<GetStarted />} />
       <Route path="/questionaire" element={<Questionaire />} />
       <Route path="/submitted" element={<Thank />} />
