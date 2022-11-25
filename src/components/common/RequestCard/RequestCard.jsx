debf8b13841d9a0f9319ebf958a4a7c14e5a3423
Bertrand <mutanganabertrand@gmail.com>
2022-11-25 09:35:06 +0200

    Add padding to card

--- src/components/common/RequestCard/RequestCard.jsx
+++ src/components/common/RequestCard/RequestCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const RequestCard = ({ request }) => {
   return (
-    <div className="drop-shadow-md border bg-white p-4 my-3 hover:bg-teal-50">
+    <div className="drop-shadow-md border bg-white p-8 my-5 hover:bg-teal-50">
       <h3 className="text-4xl text-gray-600 font-bold">{request['Name']}</h3>
-      <span className="text-2xl text-gray-500">Sent: {request['date']}</span>
-      <p className="text-2xl text-gray-500">From: {request['Email']}</p>
+      <span className="text-2xl text-gray-500 mb-1">Sent On: {request['date']}</span>
+      <p className="text-2xl text-gray-500 mb-1">From: {request['Email']}</p>
     </div>
   )
 }
--- src/components/dashboard/TopBar.jsx
+++ src/components/dashboard/TopBar.jsx
@@ -27,7 +27,7 @@ const TopBar = ({name, photoUrl}) => {
     <>
         <nav className='h-[9%] W-[100%] w-[100%] md:w-[75%] flex items-center justify-between pl-2 pr-9 drop-shadow-2xl border-b-2 absolute right-0'>
             <h1 className='md:text-4xl font-bold text-gray-600'>{name} Dashboard</h1>
-            <div className="relative flex items-center justify-center bg-gray-200 rounded-full border-2 border-green-400 cursor-pointer" onClick={handleClick}>
+            <div className="relative flex items-center justify-center bg-gray-200 rounded-full border-2 border-gray-200 cursor-pointer" onClick={handleClick}>
                 <RiArrowLeftSFill className={`absolute left-[-2.4rem] ${ showLogout ? '' : 'rotate-[-90deg]' } text-5xl text-gray-600`} />
                 <img src={photoUrl} alt="user" className="w-20 h-20 rounded-full" />
                 {showLogout && <div  className='absolute left-[-8rem] bg-gray-300 w-24 h-14 rounded-lg flex items-center justify-center hover:bg-gray-200' onClick={handleSignOut}>Logout</div>}
